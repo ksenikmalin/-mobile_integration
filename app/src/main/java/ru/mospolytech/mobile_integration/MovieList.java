6fcb51243d6ffee22a2e75ddcc9de504c725f794
ksenikmalin <74586327+ksenikmalin@users.noreply.github.com>
2021-02-02 01:40:02 +0300

    add commit

--- app/src/main/java/ru/mospolytech/mobile_integration/MovieList.java
+++ app/src/main/java/ru/mospolytech/mobile_integration/MovieList.java
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class MovieList {
-    @SerializedName("results")
-    List<MovieDetails> movies;
+    //Сериализатор для получения объекта из json в java
+    @SerializedName("results") // имя массива results (https://api.themoviedb.org/3/trending/movie/week?api_key=cc870e20173a7d922f25f0c963ff3668)
+    List<MovieDetails> movies; //преобразуем список в список объектов типа MovieDetails
 }
